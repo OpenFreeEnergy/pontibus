7fc5e93a8e95be0b3fb7199bd9eadd279a9ac9cb
Irfan Alibay <IAlibay@users.noreply.github.com>
2025-02-18 07:13:58 +0000

    Add extra test, and lint

--- src/pontibus/tests/protocols/solvation/test_dry_run.py
+++ src/pontibus/tests/protocols/solvation/test_dry_run.py
@@ -6,13 +6,15 @@ import mdtraj as mdt
 import pytest
 from gufe import ChemicalSystem
 from openff.units import unit
+from openff.units.openmm import ensure_quantity
+from openmm import NonbondedForce
 
 from pontibus.components import ExtendedSolventComponent
 from pontibus.protocols.solvation import ASFEProtocol, ASFESolventUnit, ASFEVacuumUnit
 
 
 @pytest.mark.parametrize("method", ["repex", "sams", "independent", "InDePeNdENT"])
-def test_dry_run_vacuum_benzene(benzene_modifications, method, tmpdir):
+def test_dry_run_vacuum_benzene(charged_benzene, method, tmpdir):
     s = ASFEProtocol.default_settings()
     s.protocol_repeats = 1
     s.vacuum_simulation_settings.sampler_method = method
@@ -23,7 +25,7 @@ def test_dry_run_vacuum_benzene(benzene_modifications, method, tmpdir):
 
     stateA = ChemicalSystem(
         {
-            "benzene": benzene_modifications["benzene"],
+            "benzene": charged_benzene,
             "solvent": ExtendedSolventComponent(),
         }
     )
@@ -57,7 +59,7 @@ def test_dry_run_vacuum_benzene(benzene_modifications, method, tmpdir):
 
 
 @pytest.mark.parametrize("experimental", [True, False])
-def test_dry_run_solv_benzene(experimental, benzene_modifications, tmpdir):
+def test_dry_run_solv_benzene(experimental, charged_benzene, tmpdir):
     s = ASFEProtocol.default_settings()
     s.protocol_repeats = 1
     s.solvent_output_settings.output_indices = "resname AAA"
@@ -69,7 +71,7 @@ def test_dry_run_solv_benzene(experimental, benzene_modifications, tmpdir):
 
     stateA = ChemicalSystem(
         {
-            "benzene": benzene_modifications["benzene"],
+            "benzene": charged_benzene,
             "solvent": ExtendedSolventComponent(),
         }
     )
@@ -105,7 +107,76 @@ def test_dry_run_solv_benzene(experimental, benzene_modifications, tmpdir):
         assert pdb.n_atoms == 12
 
 
-def test_dry_run_solv_benzene_opc(benzene_modifications, tmpdir):
+def test_dry_run_benzene_in_benzene_user_charges(charged_benzene, tmpdir):
+    """
+    A basic user charges test - i.e. will it retain _some_ charges passed
+    through.
+
+    TODO: something a bit more intensive.
+    """
+    s = ASFEProtocol.default_settings()
+    s.protocol_repeats = 1
+    s.solvent_output_settings.output_indices = "resname AAA"
+    s.solvation_settings.assign_solvent_charges = True
+
+    protocol = ASFEProtocol(
+        settings=s,
+    )
+
+    stateA = ChemicalSystem(
+        {
+            "benzene": charged_benzene,
+            "solvent": ExtendedSolventComponent(solvent_molecule=charged_benzene),
+        }
+    )
+
+    stateB = ChemicalSystem(
+        {
+            "solvent": ExtendedSolventComponent(solvent_molecule=charged_benzene),
+        }
+    )
+
+    # Create DAG from protocol, get the vacuum and solvent units
+    # and eventually dry run the first solvent unit
+    dag = protocol.create(
+        stateA=stateA,
+        stateB=stateB,
+        mapping=None,
+    )
+    prot_units = list(dag.protocol_units)
+
+    sol_unit = [u for u in prot_units if isinstance(u, ASFESolventUnit)]
+
+    with tmpdir.as_cwd():
+        sampler = sol_unit[0].run(dry=True)["debug"]["sampler"]
+
+        system = sampler._thermodynamic_states[0].system
+
+        # Should be benzenes all the way down
+        assert system.getNumParticles() % 12 == 0
+
+        nonbond = [f for f in system.getForces() if isinstance(f, NonbondedForce)]
+
+        assert len(nonbond) == 1
+
+        # loop through the benzene atoms
+        # partial charge is stored in the offset
+        prop_chgs = charged_benzene.to_openff().partial_charges
+        for i in range(12):
+            offsets = nonbond[0].getParticleParameterOffset(i)
+            c = ensure_quantity(offsets[2], "openff")
+
+            assert pytest.approx(c) == prop_chgs[i]
+
+        for i in range(12, system.getNumParticles()):
+            param = nonbond[0].getParticleParameters(i)
+            c = ensure_quantity(param[0], "openff")
+
+            benzene_idx = i % 12
+            assert pytest.approx(c) == prop_chgs[benzene_idx]
+
+
+def test_dry_run_solv_benzene_opc(charged_benzene, tmpdir):
     # TODO: validation tests
     # - hmass
     # - timestep
@@ -124,7 +195,7 @@ def test_dry_run_solv_benzene_opc(benzene_modifications, tmpdir):
 
     stateA = ChemicalSystem(
         {
-            "benzene": benzene_modifications["benzene"],
+            "benzene": charged_benzene,
             "solvent": ExtendedSolventComponent(),
         }
     )
@@ -195,10 +266,9 @@ def test_confgen_fail_AFE(benzene_modifications, tmpdir):
 
 """
 Different solvents
-  - Implicit
-  - Explicit
-  - Virtual sites
+  - Implicitly defined (i.e. smiles only)
+  - Virtual sites (checking you have the right vsites and parameters)
 Partial Charges
-  - Implicit
-  - Explicit
+  - Implicit (calculated on the fly)
+  - Explicit (something a bit more comprehensive than the benzene in benzene test)
 """
