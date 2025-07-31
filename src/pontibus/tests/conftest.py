5b6cf4e758c7b759292740a1076f517206baf0ab
IAlibay <IAlibay@users.noreply.github.com>
2025-07-31 17:37:24 +0100

    fix up importlib context manager usage

--- src/pontibus/tests/conftest.py
+++ src/pontibus/tests/conftest.py
@@ -1,7 +1,6 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/openfe
 
-import importlib
 import os
 from importlib import resources
 
